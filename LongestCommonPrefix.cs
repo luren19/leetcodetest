936b06125b5c2a276d7c3bbec65b19fb150b68be
luren19 <34499172+luren19@users.noreply.github.com>
2019-03-15 19:18:09 +0800

    Update LongestCommonPrefix.cs
    Divide and conquer

--- LongestCommonPrefix.cs
+++ LongestCommonPrefix.cs
@@ -31,3 +31,38 @@ public class Solution {
     }
 }
 
+public class SolutionDivideAndConquer {
+    public string LongestCommonPrefix(string[] strs) 
+    {
+        if(strs.Length == 0) return "";
+        return LongestCommonPrefix(strs, 0, strs.Length -1);      
+    }
+    
+    private string LongestCommonPrefix(string[] strs, int l, int r)
+    {
+        if(l == r)
+        {
+           return strs[l]; 
+        }
+        else
+        {
+            int mid = (l+r)/2;
+            string lcpLeft = LongestCommonPrefix(strs, l, mid);
+            string lcpRight = LongestCommonPrefix(strs, mid+1, r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+    
+    private string commonPrefix(string left, string right)
+    {
+        int min = Math.Min(left.Length, right.Length);
+        for(int i = 0; i<min; i++)
+        {
+           if(left[i].CompareTo(right[i])!=0)
+               return left.Substring(0,i);
+        }
+        return left.Substring(0,min);
+            
+    }
+}
+
