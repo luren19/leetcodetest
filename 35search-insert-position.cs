2ddc21d0c40cd826bf869e1fef68826c4285699e
luren19 <34499172+luren19@users.noreply.github.com>
2019-06-28 19:28:01 +0800

    add binary search

--- 35search-insert-position.cs
+++ 35search-insert-position.cs
@@ -15,4 +15,48 @@ public class Solution {
         }
         return i;        
     }
+    
+    public int SearchInsert2(int[] nums, int target) {
+        if(nums.Length == 0)
+        {
+            return 0;
+        }        
+        int result = BinarySearch(nums,target,0,nums.Length-1);
+        return result;
+    }
+    
+    public int BinarySearch(int[] nums, int target,int sindex,int eindex)
+    {
+        //Console.WriteLine(sindex + " " + eindex);
+        if(sindex == eindex)
+        {
+            if(nums[sindex]>=target)
+            {
+                return sindex;
+            }
+            else
+            {
+                return sindex+1;
+            }
+        }
+        int mindex = (eindex-sindex)/2 + sindex;
+        if(nums[mindex]==target)
+        {
+            return mindex;
+        }
+        else if(nums[mindex]<target)
+        {
+            sindex = mindex + 1;
+            return BinarySearch(nums,target,sindex,eindex);
+        }
+        else
+        {
+            eindex = (mindex-1);
+            if(eindex < sindex)
+            {
+                eindex = sindex;
+            }
+            return BinarySearch(nums,target,sindex,eindex);
+        }
+    }
 }
