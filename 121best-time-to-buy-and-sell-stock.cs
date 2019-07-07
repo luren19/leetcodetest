fdc6e79b80aa0b848d3943c86873a87d9d71460c
luren19 <34499172+luren19@users.noreply.github.com>
2019-07-07 10:25:33 +0800

    DP

--- 121best-time-to-buy-and-sell-stock.cs
+++ 121best-time-to-buy-and-sell-stock.cs
@@ -15,4 +15,27 @@ public class Solution {
         }
         return maxprofit;
     }
+    
+    public int MaxProfit(int[] prices) {
+        int maxprofit = 0;
+        int len = prices.Length;
+        if(len<2)
+        {
+            return maxprofit;
+        }
+        int minprice = prices[0];
+        for(int i=1;i<len;i++)
+        {
+            int tempprice = prices[i];
+            if(minprice>tempprice)
+            {
+                minprice = tempprice;
+            }
+            else if(prices[i]-minprice>maxprofit)
+            {
+                maxprofit = prices[i]-minprice;
+            }
+        }
+        return maxprofit;
+    }
 }
