0df227b1d259050c73ec7e8cebed8df034b71d58
luren19 <34499172+luren19@users.noreply.github.com>
2019-07-19 20:27:37 +0800

    Update 69sqrtx.cs

--- 69sqrtx.cs
+++ 69sqrtx.cs
@@ -22,4 +22,31 @@ public class Solution {
         }
         return Convert.ToInt32(left-1);
     }
+    
+    public int MySqrt2(int x) {
+        if(x<=1)
+        {
+            return x;
+        }
+        long left = 1;
+        long right = x;
+        while(true)
+        {
+            long mid = (right+left)/2;
+            //Console.WriteLine($"{left},{right},{mid}");
+            if(mid>x/mid)
+            {
+                right = mid-1;
+            }
+            else
+            {
+                if((mid+1)>x/(mid+1))
+                {
+                    return Convert.ToInt32(mid);
+                }
+                left = mid + 1;
+            }
+            //Console.WriteLine($"{left},{right},{mid}");
+        }
+    }
 }
