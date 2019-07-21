c8d92ab9ade757e8c0f918875e60a704b61c0dfd
luren19 <34499172+luren19@users.noreply.github.com>
2019-07-21 21:33:47 +0800

    add DP logic

--- 70climbing-stairs.cs
+++ 70climbing-stairs.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public int ClimbStairs(int n) {
+    public int ClimbStairs-Fibonacci(int n) {
         int first = 1;
         int second = 2;
         int third = n;
@@ -10,5 +10,20 @@ public class Solution {
             second = third;
         }
         return third;
+    }
+    
+    public int ClimbStairs-dp(int n) {
+        if(n<=2)
+        {
+            return n;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
     }
 }
