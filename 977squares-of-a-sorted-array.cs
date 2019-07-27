9990eaa55b658a0430a4130f012405d801f56a27
luren19 <34499172+luren19@users.noreply.github.com>
2019-07-27 15:50:31 +0800

    use Math.Abs to decide from big to small result

--- 977squares-of-a-sorted-array.cs
+++ 977squares-of-a-sorted-array.cs
@@ -38,4 +38,25 @@ public class Solution {
         
         return result;
     }
+    
+    public int[] SortedSquares-2(int[] A) {
+        int len = A.Length;
+        int i=0;
+        int j=len-1;
+        int[] result = new int[len];
+        for(int k=len-1;k>=0;k--)
+        {
+            if(Math.Abs(A[i])>Math.Abs(A[j]))
+            {
+                result[k]=A[i]*A[i];
+                i++;
+            }
+            else
+            {
+                result[k]=A[j]*A[j];
+                j--;
+            }            
+        }
+        return result;
+    }
 }
