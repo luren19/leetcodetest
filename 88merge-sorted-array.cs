49d9563e84f19ba3922d0c7f0340b0b91bf703bd
luren19 <34499172+luren19@users.noreply.github.com>
2019-07-27 15:07:58 +0800

    remove unnecessary logic

--- 88merge-sorted-array.cs
+++ 88merge-sorted-array.cs
@@ -2,30 +2,23 @@ public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
         int i=m-1;
         int j=n-1;
+        int k=m+n-1;
         while(i>=0&&j>=0)
         {
             if(nums1[i]>nums2[j])
             {
-                nums1[i+j+1]=nums1[i--];
+                nums1[k--]=nums1[i--];
             }
             else
             {
-                nums1[i+j+1]=nums2[j--];
+                nums1[k--]=nums2[j--];
             } 
         }
-        if(i<0)
+
+        while(j>=0)
         {
-            while(j>=0)
-            {
-                nums1[i+j+1]=nums2[j--];
-            }
+            nums1[k--]=nums2[j--];
         }
-        else
-        {
-            while(i>=0)
-            {
-                nums1[i+j+1]=nums1[i--];
-            }
-        }        
+                
     }
 }
