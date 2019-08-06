de559d4c8a60314526c475263f12b05046b24abd
luren19 <34499172+luren19@users.noreply.github.com>
2019-08-06 22:36:00 +0800

    use StringBuilder to reduce string concatenation

--- 257binary-tree-paths.cs
+++ 257binary-tree-paths.cs
@@ -32,4 +32,35 @@ public class Solution {
             searchRoot2Leaf(result,path+root.val+"->",root.right);
         }
     }
+    
+    public IList<string> BinaryTreePaths-usesb(TreeNode root) {
+        IList<string> result = new List<string>();
+        if(root!=null)
+        {
+            StringBuilder sb = new StringBuilder();
+            searchRoot2Leaf-usesb(result,sb,root);
+        }
+        return result;
+    }
+    
+    public void searchRoot2Leaf-usesb(IList<string> result, StringBuilder path, TreeNode root)
+    {
+        int len1 = path.Length;
+        path.Append(root.val);
+        if(root.left == null && root.right == null)
+        {
+            result.Add(path.ToString());
+        }
+        path.Append("->");
+        int len2 = path.Length;
+        if(root.left!=null)
+        {            
+            searchRoot2Leaf(result,path,root.left);
+        }
+        if(root.right!=null)
+        {
+            searchRoot2Leaf(result,path,root.right);
+        }
+        path.Remove(len1,len2-len1);
+    }
 }
