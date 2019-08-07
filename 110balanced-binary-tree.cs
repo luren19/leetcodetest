f30b36e4b5f4dd050e1da91e4d9bcd45906243c2
luren19 <34499172+luren19@users.noreply.github.com>
2019-08-07 23:54:01 +0800

    using DFS , bottom up

--- 110balanced-binary-tree.cs
+++ 110balanced-binary-tree.cs
@@ -26,4 +26,32 @@ public class Solution {
         }
         return Math.Max(depth(root.left),depth(root.right))+1;
     }
+    
+    public bool IsBalanced-dfs(TreeNode root) {
+        return dfsHeight(root)!=-1;
+    }
+    
+    public int dfsHeight(TreeNode root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int left = dfsHeight(root.left);
+        if(left == -1)
+        {
+            return -1;
+        }
+        int right = dfsHeight(root.right);
+        if(right == -1)
+        {
+            return -1;
+        }
+        if(Math.Abs(left-right)>1)
+        {
+            return -1;
+        }
+        
+        return Math.Max(left,right)+1;
+    }
 }
