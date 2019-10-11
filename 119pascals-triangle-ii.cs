f3d8cf63a5ac193324132b38c502c6b64e822cdd
luren19 <34499172+luren19@users.noreply.github.com>
2019-10-11 15:05:57 +0800

    Update and rename 11pascals-triangle-ii.cs to 119pascals-triangle-ii.cs

--- 119pascals-triangle-ii.cs
+++ 119pascals-triangle-ii.cs
@@ -13,4 +13,18 @@ public class Solution {
         
         return row;
     }
+    
+    public IList<int> GetRow2(int rowIndex) {        
+        List<int> row = new List<int>();
+        for(int rowNum = 0; rowNum <= rowIndex;rowNum++)
+        {
+            row.Add(1);
+            for(int j = rowNum-1; j >=1; j--)
+            {
+                row[j] = row[j] + row[j-1];
+            }
+        }
+        
+        return row;
+    }
 }
