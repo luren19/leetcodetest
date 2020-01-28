3546931c96defbdfb9d5305c6ff97cd73e45e504
luren19 <34499172+luren19@users.noreply.github.com>
2020-01-28 14:58:58 +0800

    Create 169majority-element.cs

---
+++ 169majority-element.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    // Use Dictionary
+    public int MajorityElement(int[] nums) {
+        Dictionary<int, int> countdic = new Dictionary<int, int>();
+        //int halflen = nums.Length/2;
+        //Console.WriteLine(halflen);
+        for(int i = 0; i < nums.Length; i++){
+            if(countdic.ContainsKey(nums[i])){
+                countdic[nums[i]]++;                
+            }
+            else{
+                countdic.Add(nums[i],1);
+            }
+            //Console.WriteLine(countdic[nums[i]]);
+            if(countdic[nums[i]] > nums.Length/2){
+                return nums[i];
+            }
+        }
+        return -1;
+
+        /*KeyValuePair<int, int> maxitem = new KeyValuePair<int, int>(0,0);
+        foreach(var item in countdic){
+            if(item.Value > maxitem.Value){
+                maxitem = item;
+            }
+        }
+        
+        return maxitem.Key;*/     
+    }
+}
