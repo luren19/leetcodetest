ea68db0ecb4d34e2631281c1d88948ad437e0658
luren19 <34499172+luren19@users.noreply.github.com>
2020-04-18 21:02:08 +0800

    Create 338counting-bits.cs

---
+++ 338counting-bits.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] CountBits(int num) {
+        if(num < 0) return null;
+        if(num == 0) return new int[1]{0};
+        int[] result = new int[num+1];
+        int recur = 1;
+        result[0] = 0;
+        result[1] = 1;
+        if(num == 1) return result;
+        for(int i = 2; i <= num; i++){
+            recur *= 2;
+            int j = 0;
+            while(j < recur && i <= num){
+                result[i++] = 1 + result[j++];
+            }
+            i--;            
+        }
+        return result;
+    }
+}
