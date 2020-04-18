4b45d71097c48fd4a1c177253c76f310de3e2a95
luren19 <34499172+luren19@users.noreply.github.com>
2020-04-18 20:22:26 +0800

    Update 191number-of-1-bits.cs

--- 191number-of-1-bits.cs
+++ 191number-of-1-bits.cs
@@ -8,4 +8,14 @@ public class Solution {
         }
         return result;
     }
+    
+    public int HammingWeight(uint n) {
+        int result = 0;
+        while(n != 0){
+            // n & (n - 1) drops the lowest set bit.
+            n &= (n-1);
+            result++;
+        }
+        return result;
+    }
 }
